e7c6fcf34bbe33097da66bdde73ebf250395b31b
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-05 15:59:28 +0100

    Make shift a case class to ensure that we can match with it

--- src/parser/ParseDecision.scala
+++ src/parser/ParseDecision.scala
@@ -5,4 +5,4 @@ import components.Token
 abstract class ParseDecision
 case class LeftReduce(root: Token, dep: Token) extends ParseDecision
 case class RightReduce(root: Token, dep: Token) extends ParseDecision
-class Shift extends ParseDecision
+case class Shift(token: Token) extends ParseDecision
