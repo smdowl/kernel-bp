e2f3481fc5d688843c9ce9c1d28b5a167c68cd0e
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-06 16:20:47 +0100

    Add nice toString methods for ParseDecisions.

--- src/parser/ParseDecision.scala
+++ src/parser/ParseDecision.scala
@@ -3,6 +3,21 @@ package parser
 import components.Token
 
 abstract class ParseDecision
-case class LeftReduce(root: Token, dep: Token) extends ParseDecision
-case class RightReduce(root: Token, dep: Token) extends ParseDecision
-case class Shift(token: Token) extends ParseDecision
+
+case class LeftReduce(root: Token, dep: Token) extends ParseDecision {
+  override def toString = {
+    s"LeftShift to add ${root.form} -> ${dep.form}"
+  }
+}
+
+case class RightReduce(root: Token, dep: Token) extends ParseDecision {
+  override def toString = {
+    s"RightShift to add ${root.form} -> ${dep.form}"
+  }
+}
+
+case class Shift(token: Token) extends ParseDecision {
+  override def toString = {
+    s"Shift ${token.form}"
+  }
+}
