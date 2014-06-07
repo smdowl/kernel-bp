10f9c91fa5ccdaf5ef5d153638e980deaaf603f5
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-07 20:50:39 +0100

    Remove test code.

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -12,18 +12,4 @@ object Main extends App {
 
   val decisionGenerator = new DecisionGenerator()
   val allParseDecisions = trees.map(decisionGenerator.generateDecisions)
-
-  val tree = TreeBuilder.buildTestTree()
-  println(tree)
-
-  def printTreeSizes() = {
-    (trees zip allParseDecisions).foreach {
-      case (tree: Tree, decisions: Seq[ParseDecision]) =>
-        //      println(tree.size)
-        //      println(decisions.length)
-        println(s"${tree.size * 2 - 2} vs ${decisions.length}")
-        println(s"${tree.numEdges * 2} vs ${decisions.length}")
-        println()
-    }
-  }
 }
