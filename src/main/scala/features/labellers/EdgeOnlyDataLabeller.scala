99792f67172ecf7dc99ea669eae9d2c8727c1da4
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-09 20:23:59 +0100

    Offload more responsibility to Context and ParseDecision.

--- src/main/scala/features/labellers/EdgeOnlyDataLabeller.scala
+++ src/main/scala/features/labellers/EdgeOnlyDataLabeller.scala
@@ -1,21 +1,31 @@
 package features.labellers
 
-import parser.ParseDecision
+import parser.{Shift, RightReduce, LeftReduce, ParseDecision, Context}
 import scala.collection.immutable.TreeSet
+import components.EmptyToken
 
 class EdgeOnlyDataLabeller extends DataLabeller {
 
-  var labelSet: TreeSet[ParseDecision] = new TreeSet()
+  var labelSet: TreeSet[ParseDecision] = new TreeSet()(new Ordering[ParseDecision] {
+    override def compare(x: ParseDecision, y: ParseDecision): Int =
+      x.hashCode() compare y.hashCode()
+  })
 
   override def initialise(decisions: Iterable[ParseDecision]): Unit = {
-    decisions.foreach(labelSet += _)
+    decisions.foreach(decision => {
+      labelSet += ParseDecision.getEmptyDecision(decision.getClass)
+    })
   }
 
   override def label(decision: ParseDecision): Int = {
-    val label = labelSet.until(decision).size
+    val label = labelSet.until(ParseDecision.getEmptyDecision(decision.getClass)).size
     assert(label < labelSet.size, "Should contain element")
     label
   }
 
-  override def getInstance(label: Int): ParseDecision = labelSet.drop(label).head
+  override def getInstance(context: Context, label: Int): ParseDecision = {
+    val emptyParseDecision = labelSet.drop(label).head
+    val decision = context.fillParseDecision(emptyParseDecision)
+    decision
+  }
 }
