e25393696fa4bd529e0810ec1bf0f7997045846f
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-09 11:08:31 +0100

    Add basic functionality tests.

--- src/main/scala/features/FeatureVector.scala
+++ src/main/scala/features/FeatureVector.scala
@@ -4,17 +4,17 @@ class FeatureVector() {
 
   private var values: Map[String, Double] = Map()
 
-  def dot(other: FeatureVector) = {
-    val output = new FeatureVector()
+  def dot(other: FeatureVector): Double = {
+    var sum = 0.0
 
     keySet.intersect(other.keySet).foreach(key => {
-      output.add(key) -> this(key) * other(key)
+      sum += this(key) * other(key)
     })
 
-    new FeatureVector
+    sum
   }
 
-  def distance(other: FeatureVector) = {
+  def distance(other: FeatureVector): Double = {
     var sum = 0.0
     (keys ++ other.keys).foreach(key => {
       sum += Math.pow(this(key) - other(key), 2)
@@ -22,16 +22,38 @@ class FeatureVector() {
     Math.pow(sum, 0.5)
   }
 
-  def add(key: String, value: Double = 1.0) = {
+  def add(key: String, value: Double = 1.0): Unit = {
     if (value == 0.0)
       values -= key
 
     values += (key -> value)
   }
 
+  def merge(other: FeatureVector): FeatureVector = {
+    val output = new FeatureVector()
+
+    (keySet ++ other.keySet).foreach(key => {
+      output.add(key) -> this(key) * other(key)
+    })
+
+    output
+  }
+
   def keys = values.keys
   def keySet = values.keySet
-  def apply(key: String) = values(key)
+  def apply(key: String) = values.getOrElse(key, 0.0)
 
-  override def toString() = values.toString()
+  override def toString = values.toString()
+}
+
+object FeatureVector extends App {
+  val v1 = new FeatureVector
+  val v2 = new FeatureVector
+
+  v1.add("a")
+  assert(v1.distance(v2) == 1.0)
+
+  v2.add("b")
+  assert(v1.dot(v2) == 0.0)
+  assert(v1.distance(v2) == Math.pow(2.0, 0.5))
 }
