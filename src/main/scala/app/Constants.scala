85c22ea7ea07e1353e291770b61e9a49ac7461a2
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-18 19:53:42 +0100

    Set to use original directory.

--- src/main/scala/app/Constants.scala
+++ src/main/scala/app/Constants.scala
@@ -4,7 +4,7 @@ object Constants {
   private final val ORIG_DIR = "/Users/shaundowling/Google Drive/UCL/master project/code/history-gen/data/orig/"
   private final val VECTOR_DIR = "/Users/shaundowling/Google Drive/UCL/master project/code/history-gen/data/vector-appended/"
 
-  private final val BASE_DIR = VECTOR_DIR
+  private final val BASE_DIR = ORIG_DIR
 
   final val MINI_TRAIN_FILE = BASE_DIR + "mini.conll"
   final val MINI_TEST_FILE = BASE_DIR + "mini-test.conll"
