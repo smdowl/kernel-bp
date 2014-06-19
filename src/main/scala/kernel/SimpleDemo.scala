52c6e8e3c1e32ed89f8e352f8b4ed9be23b8e68d
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-19 15:47:38 +0100

    Moved more constants inside the data sampling step.

--- src/main/scala/kernel/SimpleDemo.scala
+++ src/main/scala/kernel/SimpleDemo.scala
@@ -7,10 +7,7 @@ case class MessageParam(lambda: Double, sig: Double)
 
 object SimpleDemo extends App {
 
-  val Vector = DenseVector
-  val Matrix = DenseMatrix
-
-  val A = Matrix(
+  val A = DenseMatrix(
     (0,1,1,0,0),
     (0,0,0,1,1),
     (0,0,0,0,0),
@@ -20,27 +17,16 @@ object SimpleDemo extends App {
   val numNodes = A.rows
   val rootNode = 1
 
-  val rootMeans = DenseVector(0,2)
-  val rootStd = 0.2
-  val p1Root = 0.5
-
-  val middleStd = 0.2
-
-  val p1Leaf = 0.5
-  val leafStd = 0.2
-
-  val nodeOrder = 0 until numNodes
-
   val sigRoot = 0.1
 
   val n = 2000
 
-  val observedList = Vector(4)
-  val observations = Vector(0)
+  val observedList = DenseVector(4)
+  val observations = DenseVector(0)
 
   val msgParam = MessageParam(0.1, 0.3)
 
-  val sampleArr = generateData()
+  val sampleArr = generateData(A, n)
 
   val f = Figure()
   for (i <- 0 until numNodes) {
@@ -51,41 +37,56 @@ object SimpleDemo extends App {
 
 //  println(sampleArr)
 
-  def generateData(): DenseMatrix[Double] = {
-    val sampArr = DenseMatrix.zeros[Double](n, numNodes)
+  def generateData(A: DenseMatrix[Int], n: Int): DenseMatrix[Double] = {
+
+    // Model parameters
+    val rootMeans = DenseVector(0,2)
+    val rootStd = 0.2
+    val p1Root = 0.5
+
+    val middleStd = 0.2
+
+    val leafStd = 0.2
+    val p1Leaf = 0.5
+
+    val numNodes = A.rows
+
+    val sampleOrder = 0 until numNodes
+
+    // Sampling step
+    val outputArray = DenseMatrix.zeros[Double](n, numNodes)
 
     for (sampleInd <- 0 until n) {
       for (nodeInd <- 0 until numNodes) {
 
-        val whichNode = nodeOrder(nodeInd)
+        val whichNode = sampleOrder(nodeInd)
         val parentInd = getParents(A, whichNode)
 
         if (parentInd.length >= 1) { // Middle or leaf
 
           if (getChildren(A, whichNode).length >= 1) {
-            val mu = sampArr(sampleInd, parentInd(0)) // Only one parent in this graph
-            sampArr(sampleInd, whichNode) = mu + middleStd * randn()
+            val mu = outputArray(sampleInd, parentInd(0)) // Only one parent in this graph
+            outputArray(sampleInd, whichNode) = mu + middleStd * randn()
           } else {
-            val mu = sampArr(sampleInd, parentInd(0))
+            val mu = outputArray(sampleInd, parentInd(0))
             val c = rand()
             if (c <= p1Leaf)
-              sampArr(sampleInd, whichNode) = mu + leafStd * randn()
+              outputArray(sampleInd, whichNode) = mu + leafStd * randn()
             else
-              sampArr(sampleInd, whichNode) = 0 + leafStd * randn()
+              outputArray(sampleInd, whichNode) = 0 + leafStd * randn()
           }
         } else { // ROOT node
           val c = rand()
           if (c <= p1Root)
-            sampArr(sampleInd, whichNode) = rootMeans(0) + rootStd * randn()
+            outputArray(sampleInd, whichNode) = rootMeans(0) + rootStd * randn()
           else
-            sampArr(sampleInd, whichNode) = rootMeans(1) + rootStd * randn()
+            outputArray(sampleInd, whichNode) = rootMeans(1) + rootStd * randn()
         }
       }
     }
 
-    sampArr
+    outputArray
   }
-
   def getParents(A: DenseMatrix[Int], idx: Int): Seq[Int] = A(::, idx).findAll(_ > 0)
   def getChildren(A: DenseMatrix[Int], idx: Int): Seq[Int] = A(idx, ::).t.findAll(_ > 0)
 }
