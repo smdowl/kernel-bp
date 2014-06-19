52e4babdfe248e9d15d22afd98e5781a8f87dd53
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-19 17:02:37 +0100

    Add cache building functionality.

--- src/main/scala/kernel/SimpleDemo.scala
+++ src/main/scala/kernel/SimpleDemo.scala
@@ -4,6 +4,7 @@ import breeze.linalg._
 import breeze.plot._
 
 case class MessageParam(lambda: Double, sig: Double)
+case class Cache(kArr: Array[Array[Matrix[Double]]], leafArr: Array[Vector[Double]])
 
 object SimpleDemo extends App {
 
@@ -20,7 +21,7 @@ object SimpleDemo extends App {
 
   val sampleArr = generateData(A, numSamples)
 
-  plotData(sampleArr)
+//  plotData(sampleArr)
 
   // Other params
   val observedList = DenseVector(4)
@@ -31,6 +32,8 @@ object SimpleDemo extends App {
   // Parzen window parameter at root
   val sigRoot = 0.1
 
+  val cache = buildCache(A, sampleArr)
+  println()
 
 
   def generateData(A: DenseMatrix[Int], n: Int): DenseMatrix[Double] = {
@@ -85,7 +88,7 @@ object SimpleDemo extends App {
   }
 
   def getParents(A: DenseMatrix[Int], idx: Int): Seq[Int] = A(::, idx).findAll(_ > 0)
-  def getChildren(A: DenseMatrix[Int], idx: Int): Seq[Int] = A(idx, ::).t.findAll(_ > 0)
+  def getChildren(A: DenseMatrix[Int], idx: Int): Seq[Int] = try {A(idx, ::).t.findAll(_ > 0)} catch {case _ => Seq[Int]()}
 
   def plotData(data: DenseMatrix[Double]) = {
     val numNodes = data.cols
@@ -96,5 +99,26 @@ object SimpleDemo extends App {
       p += hist(data(::, i))
       p.title = s"Node $i"
     }
+  }
+
+  def buildCache(A: DenseMatrix[Int], sampleArr: DenseMatrix[Double]): Cache = {
+    val kArr = Array.ofDim[Matrix[Double]](numNodes, numNodes)
+    val leafArr = Array.ofDim[Vector[Double]](numNodes)
+
+    for (nodeInd <- 0 until numNodes) {
+      val children = getChildren(A, nodeInd)
+      for (childInd <- children)
+        kArr(nodeInd)(childInd) = DenseMatrix.zeros[Double](1,1)
+
+      if (children.length == 0) {
+        kArr(nodeInd)(nodeInd) = DenseMatrix.zeros[Double](1,1)
+        leafArr(nodeInd) = sampleArr(::, nodeInd)
+      }
+
+      for (parentInd <- getParents(A, nodeInd))
+        kArr(nodeInd)(parentInd) = DenseMatrix.zeros[Double](1,1)
+    }
+
+    Cache(kArr, leafArr)
   }
 }
