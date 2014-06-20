ac886da5ce294f139a6934fb0514fa5b9e505686
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-20 14:56:14 +0100

    Add option to provide a different adj matrix when finding parents/children.

--- src/main/scala/kernel/Model.scala
+++ src/main/scala/kernel/Model.scala
@@ -10,8 +10,8 @@ abstract class Model(val n: Int) {
   def numNodes: Int = A.rows
   def getPrunedTree(observedNodes: Set[Int]): (DenseMatrix[Int], Set[Int])
 
-  def getParents(nodeId: Int): Seq[Int] = A(::, nodeId).findAll(_ > 0)
-  def getChildren(nodeId: Int): Seq[Int] = try {A(nodeId, ::).t.findAll(_ > 0)} catch {case _ => Seq[Int]()}
+  def getParents(nodeId: Int, A: DenseMatrix[Int] = this.A): Seq[Int] = A(::, nodeId).findAll(_ > 0)
+  def getChildren(nodeId: Int, A: DenseMatrix[Int] = this.A): Seq[Int] = try {A(nodeId, ::).t.findAll(_ > 0)} catch {case _ => Seq[Int]()}
 
   def generateData(): DenseMatrix[Double]
 }
