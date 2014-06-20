e5618b0e7e7ca54c6b7de5df730a8b35152732af
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-20 10:40:48 +0100

    Finish first inference part.

--- src/main/scala/kernel/SimpleDemo.scala
+++ src/main/scala/kernel/SimpleDemo.scala
@@ -7,7 +7,6 @@ case class MessageParam(lambda: Double, sig: Double)
 case class Cache(kArr: Array[Array[DenseMatrix[Double]]], leafArr: Array[Vector[Double]])
 
 object SimpleDemo {
-
   def main(args: Array[String]) = {
 
     val numSamples = 200
@@ -15,17 +14,34 @@ object SimpleDemo {
 
     val sampleArr = model.generateData()
 
-    plotData(sampleArr)
+//    plotData(sampleArr)
 
     // Other params
-    val observedList = DenseVector(4)
-    val observations = DenseVector(0)
+    val observedList = Seq(4)
+    val observations = Seq(0)
 
     // Parzen window parameter at root
     val sigRoot = 0.1
 
-//    val cache = buildCache(A, sampleArr, msgParam)
-    println()
+    val cache = buildCache(model, sampleArr)
+
+    // INFERENCE START
+
+    val betaArr = Array.ofDim[Vector[Double]](model.numNodes - observedList.length)
+
+    for ((leafId, observation) <- observedList zip observations) {
+      val parentId = model.getParents(leafId)(0)
+
+      val Kt = cache.kArr(leafId)(leafId)
+      val Ks = cache.kArr(leafId)(parentId)
+      val I = DenseMatrix.eye[Double](Kt.rows)
+
+      val kt = rbfDot(cache.leafArr(leafId), DenseVector(observation), model.msgParam.sig)
+
+      // Have to split because type seems not to be infered otherwise
+      val left: DenseMatrix[Double] = Kt :+ I * model.msgParam.lambda
+      betaArr(leafId) = (left * (Ks + I * model.msgParam.lambda)) \ kt
+    }
   }
 
   def plotData(data: DenseMatrix[Double]) = {
@@ -73,23 +89,5 @@ object SimpleDemo {
         out(i, j) = Math.exp(- Math.pow(p1(i) - p2(j), 2) / (2 * Math.pow(deg, 2)))
 
     out
-  }
-
-  def other() = {
-    //  val betaArr = Array.ofDim[Vector[Double]](numNodes - observedList.length)
-
-    //  for ((leafInd, observation) <- observedList zip observations) {
-    //    val parentInd = getParents(A, leafInd)(0)
-    //
-    //    val Kt = cache.kArr(leafInd)(leafInd)
-    //    val Ks = cache.kArr(leafInd)(parentInd)
-    //    val I = DenseMatrix.eye[Double](Kt.rows)
-    //
-    //    val kt = rbfDot(cache.leafArr(leafInd), DenseVector(observation), msgParam.sig)
-    //    println()
-    ////    val obj = (Kt :+ I * msgParam.lambda)
-    ////    obj
-    ////    betaArr(leafInd) = (obj * (Ks + I * msgParam.lambda)) \ kt
-    //  }
   }
 }
