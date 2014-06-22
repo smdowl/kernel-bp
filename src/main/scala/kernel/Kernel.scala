87cf9e28bc9086f0f6b8cd521e0907829354391d
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-22 14:07:39 +0100

    Start work on new version.

--- src/main/scala/kernel/Kernel.scala
+++ src/main/scala/kernel/Kernel.scala
@@ -1,20 +1,47 @@
 package kernel
 
-import breeze.linalg.{DenseMatrix, Vector}
+import breeze.linalg._
 
 trait Kernel {
-  def apply(p1: Vector[Double], p2: Vector[Double], deg: Double = 1.0): DenseMatrix[Double]
+  def apply(p1: DenseVector[Double], p2: DenseVector[Double], deg: Double = 1.0): DenseMatrix[Double]
+}
+
+object RBFKernel extends App {
+  val kernel = new RBFKernel
+
+  val p1 = DenseVector(1.0, 2.0, 3.0)
+  kernel(p1, p1, 0.5)
 }
 
 class RBFKernel extends Kernel {
-  override def apply(p1: Vector[Double], p2: Vector[Double], deg: Double = 1.0): DenseMatrix[Double] = {
-    // TODO: Currently slow version, but fast version is in MATLAB code
-    val out = DenseMatrix.zeros[Double](p1.length, p2.length)
+  override def apply(p1Vec: DenseVector[Double], p2Vec: DenseVector[Double], deg: Double = 1.0): DenseMatrix[Double] = {
 
-    for (i <- 0 until p1.length)
-      for (j <- 0 until p2.length)
-        out(i, j) = Math.exp(- Math.pow(p1(i) - p2(j), 2) / (2 * Math.pow(deg, 2)))
+    val n1 = p1Vec.length
+    val n2 = p2Vec.length
 
-    out
+    val p1 = DenseMatrix(p1Vec.toArray)
+    val p2 = DenseMatrix(p2Vec.toArray)
+
+    val p1Sq = p1 :* p1
+
+    val G: DenseMatrix[Double] = sum(p1 :* p1, Axis._0).asInstanceOf[DenseVector[Double]].asDenseMatrix.t
+    val H: DenseMatrix[Double] = sum(p2 :* p2, Axis._0).asInstanceOf[DenseVector[Double]].asDenseMatrix.t
+
+    val Q = repmat(G, 1, n2)
+    val R = repmat(H.t, n1, 1)
+
+    val T: DenseMatrix[Double] = Q + R
+    val r: DenseMatrix[Double] = p1 * p2.t
+
+    T - r * 2.0
+  }
+
+  private def repmat(A: DenseMatrix[Double], m: Int, n: Int): DenseMatrix[Double] = {
+    assert(m > 0)
+    assert(n > 0)
+
+    val row: DenseMatrix[Double] = DenseMatrix.horzcat((for (i <- 0 until n) yield A):_*)
+
+    DenseMatrix.vertcat((for (j <- 0 until m) yield row):_*)
   }
 }
