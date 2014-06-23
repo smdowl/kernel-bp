a4dc92d0ace3f8e3ce9d19b48781c267bf91619f
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-23 21:22:01 +0100

    Add bound option to method call.

--- src/main/scala/kernel/linalg/package.scala
+++ src/main/scala/kernel/linalg/package.scala
@@ -3,8 +3,7 @@ package kernel
 import breeze.linalg.{DenseVector, DenseMatrix, all}
 
 package object linalg {
-  def nearlyEqual(m1: DenseMatrix[Double], m2: DenseMatrix[Double]): Boolean = {
-    val bound = 1.0e-5
+  def nearlyEqual(m1: DenseMatrix[Double], m2: DenseMatrix[Double], bound: Double=1.0e-5): Boolean = {
     all(m1 :< m2 + bound) && all(m1 :> m2 - bound)
   }
 
