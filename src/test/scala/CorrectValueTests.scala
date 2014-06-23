3fb236da2b8a14c91129beb9861a0e0a666d198a
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-23 21:24:29 +0100

    Change tests to increase re-use and make results more interpretable.

--- src/test/scala/CorrectValueTests.scala
+++ src/test/scala/CorrectValueTests.scala
@@ -4,6 +4,7 @@ import io.MatrixReader
 import kernel._
 import kernel.linalg.{vec2mat, nearlyEqual}
 import org.scalatest.{BeforeAndAfter, FunSuite, Matchers}
+import scala.util.Random
 
 class CorrectValueTests extends FunSuite with Matchers with BeforeAndAfter {
   var model: Model = _
@@ -32,7 +33,7 @@ class CorrectValueTests extends FunSuite with Matchers with BeforeAndAfter {
     nearlyEqualArrays(c1.leafArr.map(vec2mat), c2.leafArr.map(vec2mat))
 
     for ((row1, row2) <- c1.kArr zip c2.kArr)
-      nearlyEqualArrays(row2, row2)
+      allNearlyEqualArrays(row1, row2)
   }
 
   def loadTestCache(): Cache = {
@@ -68,23 +69,62 @@ class CorrectValueTests extends FunSuite with Matchers with BeforeAndAfter {
     row.toArray
   }
 
-  test("Betas match") {
-    testBetaSimilarity(model, betaArr)
-  }
-
-  def testBetaSimilarity(model: Model, betaArr: Array[DenseMatrix[Double]]) = {
+  test("Any betas match") {
     val trueArr = model.loadCorrect()
-    nearlyEqualArrays(trueArr, betaArr)
+    anyNearlyEqualArrays(trueArr, betaArr)
   }
 
-  def nearlyEqualArrays(a1: Array[DenseMatrix[Double]], a2: Array[DenseMatrix[Double]]) = {
+  test("All betas match") {
+    val trueArr = model.loadCorrect()
+    allNearlyEqualArrays(trueArr, betaArr)
+  }
+
+  def allNearlyEqualArrays(a1: Array[DenseMatrix[Double]], a2: Array[DenseMatrix[Double]]) = {
+    nearlyEqualArrays(a1, a2).forall(_ == true) shouldBe true
+  }
+
+  def anyNearlyEqualArrays(a1: Array[DenseMatrix[Double]], a2: Array[DenseMatrix[Double]]) = {
+    nearlyEqualArrays(a1, a2).exists(_ == true) shouldBe true
+  }
+
+  def nearlyEqualArrays(a1: Array[DenseMatrix[Double]], a2: Array[DenseMatrix[Double]]): Array[Boolean] = {
     a1.length shouldEqual a2.length
 
-    for ((m1, m2) <- a1 zip a2) {
-      if (m1 == null || m2 == null)
-        m1 shouldEqual m2
-      else
-        nearlyEqual(m1, m2) shouldBe true
+    val matches = (a1 zip a2).map {
+      case (m1: DenseMatrix[Double], m2: DenseMatrix[Double]) =>
+          nearlyEqual(m1, m2, 1.0e-2)
+      case t: (Any, Any) => t._1 == t._2
     }
+
+    val (n, m) = getRandomElementInMatrix(a1)
+
+    for (i <- 0 until a1.length) {
+      println(matches(i), nullOrElementAtIndex(a1(i), n, m), nullOrElementAtIndex(a2(i), n, m))
+    }
+
+    matches
+  }
+
+  def getRandomElementInMatrix(arr: Array[DenseMatrix[Double]]) = {
+    val (rows, cols) = sizeOfMatricesInArray(arr)
+    (Random.nextInt(rows), Random.nextInt(cols))
+  }
+
+  def sizeOfMatricesInArray(arr: Array[DenseMatrix[Double]]) = {
+    var i = 0
+    while (i < arr.length && arr(i) == null)
+      i += 1
+
+    if (i < arr.length)
+      (arr(i).rows, arr(i).cols)
+    else
+      (0, 0)
+  }
+
+  def nullOrElementAtIndex(m: DenseMatrix[Double], i: Int, j: Int) = {
+    if (m != null)
+      m(i,j)
+    else
+      null
   }
 }
