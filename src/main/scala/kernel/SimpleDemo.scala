b10aa8ac853664e69a6d043c1c0888ed31328524
Shaun Dowling <shaunmdowling@googlemail.com>
2014-06-24 12:03:25 +0100

    Create a new test function that is free to change.

--- src/main/scala/kernel/SimpleDemo.scala
+++ src/main/scala/kernel/SimpleDemo.scala
@@ -2,6 +2,7 @@ package kernel
 
 import breeze.linalg._
 import kernel.plotting.Plotter
+import app.Constants
 
 case class MessageParam(lambda: Double, sig: Double)
 
@@ -34,7 +35,26 @@ object SimpleDemo {
     Plotter.plotResults(result)
   }
 
+  def test() = {
+    val numSamples = 400
+    val model: Model = new DemoModel(numSamples, Constants.SAMPLE_DATA)
+
+    val sampleArr = model.generateData()
+    val observations = Map(3 -> DenseVector(2.0))
+    Plotter.plotData(sampleArr)
+
+    val kernel = new RBFKernel()
+    val passer = new MessagePasser(model, kernel)
+
+    val betaArr = passer.passMessages(sampleArr, observations)
+
+//    val axisBelief = linspace(-5, 5, 200)
+//    val sigRoot = 0.1     // Parzen window parameter at root
+//
+//    val result = new Result(model, kernel, sampleArr, observations, betaArr, sigRoot, axisBelief)
+  }
+
   def main(args: Array[String]): Unit = {
-    runDemo()
+    test()
   }
 }
