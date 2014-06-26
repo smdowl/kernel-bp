f3941044dda620702390930deb0c3a11a60806f7
Shaun Dowling <Shaun@Shauns-MacBook-Pro.local>
2014-06-26 17:19:55 +0100

    Remove unused code.

--- src/main/scala/io/MatrixWriter.scala
+++ src/main/scala/io/MatrixWriter.scala
@@ -7,9 +7,6 @@ import com.github.tototoshi.csv._
 
 object MatrixWriter {
   def writeMatrixToFile(filepath: String, matrix: DenseMatrix[Double]) = {
-//    val write = matrix.toString()
-//    s"echo $write" #> new File(filepath) !
-
     val writer = CSVWriter.open(filepath)
     for (i <- 0 until matrix.rows) {
       val row: DenseVector[Double] = matrix(i, ::).t
