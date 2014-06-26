4c3397c7b8800b253410e7d542a7fd33d84bf4f7
Shaun Dowling <Shaun@Shauns-MacBook-Pro.local>
2014-06-26 20:28:15 +0100

    Set up simple higher dim demo.

--- src/main/scala/kernel/SimpleDemo.scala
+++ src/main/scala/kernel/SimpleDemo.scala
@@ -1,7 +1,7 @@
 package kernel
 
 import breeze.linalg._
-import kernel.models.{DemoModel, Model}
+import kernel.models.{ExtendedModel, DemoModel, Model}
 import kernel.plotting.Plotter
 import app.Constants
 
@@ -56,7 +56,28 @@ object SimpleDemo {
     Plotter.plotResults(result)
   }
 
+  def higherDim() = {
+    val numSamples = 20
+    val model: ExtendedModel = new ExtendedModel(numSamples)
+
+    val sampleArr = model.generateData()
+
+    val observations = Map(3 -> DenseVector(2.0, 2.0))
+//    Plotter.plotData(sampleArr)
+
+//    val kernel = new RBFKernel()
+//    val passer = new MessagePasser(model, kernel)
+//
+//    val betaArr = passer.passMessages(sampleArr, observations)
+//
+//    val axisBelief = linspace(-5, 5, 200)
+//    val sigRoot = 0.1     // Parzen window parameter at root
+//
+//    val result = new Result(model, kernel, sampleArr, observations, betaArr, sigRoot, axisBelief)
+//    Plotter.plotResults(result)
+  }
+
   def main(args: Array[String]): Unit = {
-    test()
+    higherDim()
   }
 }
