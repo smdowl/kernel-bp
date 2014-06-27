a1a68f7920f721187bb9fee53813e8f6bb365cbe
Shaun Dowling <Shaun@Shauns-MacBook-Pro.local>
2014-06-27 14:00:27 +0100

    Add meshgrid function.

--- src/main/scala/kernel/linalg/package.scala
+++ src/main/scala/kernel/linalg/package.scala
@@ -13,4 +13,17 @@ package object linalg {
     else
       DenseMatrix(vec.toArray)
   }
+
+  def meshgrid(x1: DenseVector[Double], x2: DenseVector[Double]): DenseMatrix[Double] = {
+    val x1Mesh = DenseMatrix.zeros[Double](x2.length, x1.length)
+    for (i <- 0 until x2.length) {
+      x1Mesh(i, ::) := x1.t
+    }
+    val x2Mesh = DenseMatrix.zeros[Double](x2.length, x1.length)
+    for (i <- 0 until x1.length) {
+      x2Mesh(::, i) := x2
+    }
+
+    DenseMatrix(x2Mesh.toDenseVector.toArray, x1Mesh.toDenseVector.toArray).t
+  }
 }
