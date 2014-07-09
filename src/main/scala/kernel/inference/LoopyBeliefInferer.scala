e79bff5638503e05531c9030de7d4bd4900cebca
Shaun <shaunmdowling@gmail.com>
2014-07-09 10:45:32 +0100

    Remove empirical belief calculation.

--- src/main/scala/kernel/inference/LoopyBeliefInferer.scala
+++ src/main/scala/kernel/inference/LoopyBeliefInferer.scala
@@ -4,45 +4,7 @@ import breeze.linalg._
 import kernel.plotting.LoopyResult
 
 class LoopyBeliefInferer(result: LoopyResult) {
-  
-  def calculateEmpiricalBelief(): DenseVector[Double] = {
-    // Empirical root belief
-    implicit val threshold = 0.01
-
-    val numObs = result.observations.keys.size
-    val condIndicesArray = Array.ofDim[Seq[Int]](numObs)
-
-    // Want to find all points that are suitably close to those that we have observed.
-    for((nodeId, index) <- result.observations.keys.zipWithIndex) {
-      val data: DenseMatrix[Double] = result.sampleArr(nodeId)
-      val nodeObservations = result.observations(nodeId)
-      val newIndices =  for (j <- 0 until data.rows if closeIndex(j, data, nodeObservations)) yield j
-
-      condIndicesArray(index) = newIndices
-    }
-
-    // TODO: Only works when conditioning on a single obs, which is actually fine for what we need
-    val condIndices = condIndicesArray(0)
-    if (condIndices.length > 0) {
-      val d = result.sampleArr(0).cols
-      val condNodes = DenseMatrix.tabulate[Double](condIndices.size, d) { (i, j) => result.sampleArr(result.model.rootNode)(condIndices(i), j)}
-
-      val beliefRootEmp = result.kernel(result.axisBelief, condNodes, result.sigRoot)
-      sum(beliefRootEmp.t(::, *)).asInstanceOf[DenseMatrix[Double]].toDenseVector
-    } else DenseVector.zeros[Double](result.axisBelief.rows)
-  }
-
-  def closeIndex(j: Int, data: DenseMatrix[Double], nodeObservations: DenseMatrix[Double])(implicit threshold: Double): Boolean = {
-    val point: DenseVector[Double] = data(j, ::).t
-
-    // TODO: Close to any of the points. Need to think if this is what we want.
-    (0 until nodeObservations.rows).exists(i => {
-      val obs = nodeObservations(i, ::).t
-      val dist: Double = norm(point - obs).asInstanceOf[Double]
-      dist < threshold
-    })
-  }
-
+ 
   def calculateKernelRootMarginal() = {
     val kernelRes = result.kernel(result.axisBelief, result.sampleArr(result.model.rootNode), result.sigRoot)
     sum(kernelRes, Axis._1)
