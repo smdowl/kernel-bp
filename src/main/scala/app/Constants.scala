183dd56821c7b4a0bc08f817ed9f2f891777f129
Shaun <shaunmdowling@gmail.com>
2014-07-13 17:06:32 +0100

    Add some more data files.

--- src/main/scala/app/Constants.scala
+++ src/main/scala/app/Constants.scala
@@ -2,7 +2,7 @@ package app
 
 object Constants {
   
-  val orig = false
+  val orig = true
   val SUBDIR: String = if (orig) "orig/" else "vector-appended/"
 
   private final val BASE_DIR = "/Users/Shaun/dev/kernel-bp/data/" + SUBDIR
@@ -12,6 +12,10 @@ object Constants {
 
   final val DEP_TEST = BASE_DIR + "dep-test.conll"
   final val TRAIN_FILE = BASE_DIR + "train.conll"
+  final val DEV_FILE = BASE_DIR + "dev.conll"
+
+  final val SMALL_TRAIN_FILE = BASE_DIR + "small-train.conll"
+  final val SMALL_DEV_FILE = BASE_DIR + "small-dev.conll"
 
   final val MATLAB_DIR = "/Users/Shaun/dev/kernelBP_source/kernelBP/"
   final val CORRECT_DIR = MATLAB_DIR + "test-output/"
