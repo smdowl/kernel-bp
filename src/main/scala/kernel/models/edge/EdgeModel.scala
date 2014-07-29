81670de44d82854795da57a45964f354a725dd59
Shaun <shaunmdowling@gmail.com>
2014-07-29 10:11:42 +0100

    Add key index to EdgeModel.

--- src/main/scala/kernel/models/edge/EdgeModel.scala
+++ src/main/scala/kernel/models/edge/EdgeModel.scala
@@ -7,4 +7,10 @@ trait EdgeModel {
   def testObservations: Array[Map[Int, DenseMatrix[Double]]]
   def testLabels: Array[Map[Int, DenseMatrix[Double]]]
   def keyArray: Array[String]
+
+  def keyIndex: Map[String, Int] = {
+    Map[String, Int]() ++ (0 until keyArray.length).map(i => {
+      this.keyArray(i) -> i
+    })
+  }
 }
