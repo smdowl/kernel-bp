887d5fdf70657ae72038b7cc2fadb08d7102fb31
Shaun <shaunmdowling@gmail.com>
2014-08-02 13:44:31 +0100

    Move more complex features to separate extractor.

--- src/main/scala/kernel/models/toys/extractors/AddedFeaturesHMMExtractor.scala
+++ src/main/scala/kernel/models/toys/extractors/AddedFeaturesHMMExtractor.scala
@@ -4,7 +4,7 @@ import computation.FeatureVector
 
 import scala.collection.mutable.Queue
 
-class SimpleHMMFeatureExtractor extends ToyFeatureExtractor {
+class AddedFeaturesHMMExtractor extends ToyFeatureExtractor {
   /**
    * Convert each set of samples into an array of feature
    */
@@ -12,9 +12,16 @@ class SimpleHMMFeatureExtractor extends ToyFeatureExtractor {
     val hidden = sampleData.slice(0, sampleData.length / 2)
     val visible = sampleData.slice(sampleData.length / 2, sampleData.length)
 
+    val history = Queue[String]()
+
     hidden.map(node => {
       val vector = new FeatureVector()
       vector.add(s"label:$node")
+      vector.add(s"history:$history")
+      history.enqueue(node)
+
+      if (history.length > 3)
+        history.dequeue()
       vector
     }) ++ visible.map(node => {
       val vector = new FeatureVector()
