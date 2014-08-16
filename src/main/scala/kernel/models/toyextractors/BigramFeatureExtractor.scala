e8349ee264cf247378771a7c0e80a137472add86
Shaun <shaunmdowling@gmail.com>
2014-08-16 18:11:42 +0100

    Make BigramExtractor.

---
+++ src/main/scala/kernel/models/toyextractors/BigramFeatureExtractor.scala
@@ -0,0 +1,32 @@
+package kernel.models.toyextractors
+
+import computation.FeatureVector
+
+class BigramFeatureExtractor extends ToyFeatureExtractor {
+  /**
+   * Convert each set of samples into an array of feature
+   */
+  override def extractFeatures(sampleData: Seq[String]): Seq[FeatureVector] = {
+    val hidden = sampleData.slice(0, sampleData.length / 2)
+    val visible = sampleData.slice(sampleData.length / 2, sampleData.length)
+
+    var prev: String = null
+    var prevForm: String = null
+
+    hidden.map(node => {
+      val vector = new FeatureVector()
+      vector.add(s"label:$node")
+      if (prev != null)
+        vector.add(s"prev-label:$prev")
+      prev = node
+      vector
+    }) ++ visible.map(node => {
+      val vector = new FeatureVector()
+      vector.add(s"form:$node")
+      if (prevForm != null)
+        vector.add(s"prev-form:$prevForm")
+      prevForm = node
+      vector
+    })
+  }
+}
